044cf0e6c144cb5df9704b7ed4783627fe2a0ded
libochao <522427432@qq.com>
2016-07-07 14:51:17 +0800

    水平垂直柱状图

---
+++ sample/app/controllers/bar_chart_controller.rb
@@ -0,0 +1,20 @@
+class BarChartController < ApplicationController
+  def index
+    @component_name = "ReactChart.BarChart"
+    @component_data = {
+      type: "horizontal",
+      bottom_name: "水果种类",
+      height_name: "销售量",
+      item_length: 50,
+      item_area_length: 120,
+      items: [
+        {name: "苹果", num: 200},
+        {name: "香蕉", num: 120},
+        {name: "梨子", num: 90},
+        {name: "葡萄", num: 220},
+        {name: "葡萄1", num: 220},
+        {name: "橘子", num: 50}
+      ]
+    }
+  end
+end
--- sample/config/routes.rb
+++ sample/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "/:controller", action: "index"
+  get "/bar_chart", to: "bar_chart#index"
 end
